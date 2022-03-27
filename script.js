98db8cad487483b5396c4ac201863fbdfb6c0271
«SvyatoslavErmolenko» <Barashko40@gmail.com>
2022-03-27 21:39:06 +0300

    Урок №2: Типы данных, операторы, методы и свойства

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-const title = "";
-const screens = "";
-const screensPrice = "";
-const rollback = "";
-const fullPrice = "";
-const adaptive = "";
+const title = "lesson02";
+console.log(typeof title); // вывод в консоль тип данных
+const screens = "Simple, Complex, Interactive";
+console.log(screens.length); // вывод в консоль длину строки
+console.log(screens.toLocaleLowerCase().split(" ")); // приводит строку к нижнему регистру и разбивает строку на массив
+const screensPrice = 182;
+console.log("\t The cost of layout screens in RUB:", screensPrice, "\n\t The cost of layout screens in USD:", screensPrice,
+    "\n\t The cost of layout screens in UAH:", screensPrice, "\n\t The cost of layout screens in CNY:", screensPrice); // выводит в консоль стоимость верстки: рублей, долларов, гривен, юани
+const rollback = 32;
+const fullPrice = 548;
+console.log(typeof fullPrice); // вывод в консоль тип данных
+console.log("\t Cost of website development in RUB:", fullPrice, "\n\t Cost of website development in USD:", fullPrice,
+    "\n\t Cost of website development in UAH:", fullPrice, "\n\t Cost of website development in CNY:", fullPrice); // выводит в консоль стоимость разработки сайта: рублей, долларов, гривен, юани
+const adaptive = true;
+console.log(typeof adaptive); // вывод в консоль тип данных
 
-alert("Hello world");
-console.log("Hello world");
+console.log(fullPrice * (rollback / 100)); // процент отката посреднику
