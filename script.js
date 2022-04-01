f0d7df8df4bc9ba30c5170108c7a7c2971bab86d
«SvyatoslavErmolenko» <Barashko40@gmail.com>
2022-04-01 15:46:22 +0300

    Урок 4. Функции, анонимные функции, callback - функции, чистые функции.

--- script.js
+++ script.js
@@ -2,60 +2,86 @@
 
 let title = prompt("Как называется ваш проект?", "Напишите название вашего проекта!");
 let screens = prompt("Какие типы экранов нужно разработать?", "Простой, сложный или интерактивный!");
-let screensPrice = prompt("Сколько будет стоить данная работа?", "Укажите цену!");
+let screensPrice = +prompt("Сколько будет стоить данная работа?", "Укажите цену!");
 let adaptive = confirm("Нужен ли адаптив на сайте?");
 let service1 = prompt("Какой дополнительный тип услуги нужен?", "Назваие!");
-let servicePrice1 = prompt("Сколько это будет стоить?", "Стоимость!");
+let servicePrice1 = +prompt("Сколько это будет стоить?", "Стоимость!");
 let service2 = prompt("Какой дополнительный тип услуги нужен?", "Назваие!");
-let servicePrice2 = prompt("Сколько это будет стоить?", "Стоимость!");
+let servicePrice2 = +prompt("Сколько это будет стоить?", "Стоимость!");
 let rollback = 32;
-let fullPrice = (+screensPrice + +servicePrice1 + +servicePrice2);
+let fullPrice = (screensPrice + servicePrice1 + servicePrice2);
 let servicePercentPrice = (fullPrice - (fullPrice * (rollback / 100)));
+let allServicePrices;
 
-
-// конструкцию условий 
-switch (true) {
-    case fullPrice >= 30000:
-        fullPrice === 30000 ? console.log("Не достаточно до скидки `( ") : console.log("Даем скидку в 10%");
+//конструкциия условий
+/*switch (true) {
+    case cena >= 30000:
+        cena === 30000 ? console.log("Даем скидку в 5%") : console.log("Даем скидку в 10%");
         break
-    case 15000 <= fullPrice && fullPrice <= 30000:
-        fullPrice === 15000 ? console.log("Не достаточно до скидки `( ") : console.log("Даем скидку в 5%");
+    case 15000 <= cena && cena <= 30000:
+        cena === 15000 ? console.log("Скидка не предусмотрена") : console.log("Даем скидку в 5%");
         break
-    case 15000 >= fullPrice && fullPrice >= 0:
-        fullPrice === 0 ? console.log("Ну тут совсем мало денюшек ((( ") : console.log("Скидка не предусмотрена");
+    case 15000 >= cena && cena >= 0:
+        cena === 0 ? console.log("Скидка не предусмотрена") : console.log("Скидка не предусмотрена");
         break
     default:
         console.log("Что то пошло не так");
 
 }
-// lesson04
+*/
 
-let getAllServicePrices = function () {
-    return +servicePrice1 + +servicePrice2;
+// функция условий скидок
+const getRollbackMessage = function (price) {
+    if (price >= 30000) {
+        return 'Даем скидку в 10%';
+    } else if (15000 <= price && price <= 30000) {
+        return 'Даем скидку в 5%';
+    } else if (15000 >= price && price >= 0) {
+        return ('Скидка не предусмотрена');
+    } else {
+        return 'Что то пошло не так';
+    }
 
 };
-let allServicePrices = getAllServicePrices();
-console.log(allServicePrices);
 
+// lesson04
+// функция указывающую тип операнда
+const showTypeof = function (variable) {
+    console.log(variable, typeof variable);
+};
+// функция возвращает сумму всех дополнительных услуг
+let getAllServicePrices = function () {
+    return servicePrice1 + servicePrice2;
+
+};
+// функция возвращает сумму стоимости верстки и стоимости дополнительных услуг
 function getFullPrise(screensPrice, allServicePrices) {
-    return +screensPrice + +allServicePrices;
-}
+    return screensPrice + allServicePrices;
+};
+// функция возвращает title меняя его таким образом: первый символ с большой буквы, остальные с маленькой с учетом пустых символов 
+let getTitle = function (str) {
+    return str.trim()[0].toUpperCase() + str.trim().substr(1).toLowerCase(1);
+
+};
+// функция возвращает итоговую стоимость за вычетом процента отката
+let getServicePercentPrices = function () {
+    return fullPrice - servicePercentPrice;
+};
+
+
+
+// переопределение переменных в функции
+allServicePrices = getAllServicePrices();
 fullPrice = getFullPrise(screensPrice, allServicePrices);
-console.log(fullPrice);
+title = getTitle(title);
+servicePercentPrice = getServicePercentPrices();
 
-let getTitle = function (newStr) {
-    if (!newStr) return newStr;
-    return newStr[0].toUpperCase() + newStr.slice(1);
-}
-title = getTitle(newStr);
-console.log(title);
+// вызовы функции showTypeOf
+showTypeof(screens);
+showTypeof(getRollbackMessage(fullPrice));
+showTypeof(servicePercentPrice);
 
-
-/*console.log(typeof title); // вывод в консоль тип данных
-console.log(screens.length); // вывод в консоль длину строки
-console.log(screens.toLocaleLowerCase().split(", ")); // приводит строку к нижнему регистру и разбивает строку на массив
-console.log(typeof fullPrice); // вывод в консоль тип данных
-console.log("\t Cost of website development in RUB:", fullPrice, "\n\t Cost of website development in USD:", fullPrice,
-    "\n\t Cost of website development in UAH:", fullPrice, "\n\t Cost of website development in CNY:", fullPrice); // выводит в консоль стоимость разработки сайта: рублей, долларов, гривен, юани */
-console.log(fullPrice * (rollback / 100)); // процент отката посреднику
-console.log(Math.ceil(servicePercentPrice));
+// console.log
+console.log(screens);
+console.log(getRollbackMessage(fullPrice));
+console.log(servicePercentPrice);
